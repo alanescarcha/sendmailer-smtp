9c98920965dd870987582a4df0a00a53f9a98668
Alan Escarcha <alanescarcha11@gmail.com>
2021-11-10 02:13:19 -0300

    First upload :)

---
+++ index.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+const port = process.env.PORT || 3000;
+let errorResponse = {
+    "code": "400",
+    "response": "Error processing the request",
+    "responseCode": "400"
+}
+
+async function sendEmail(host, port, secure, user, password, name, from, to, subject, typemsg, message) {
+
+    if (secure == 'ssl') {
+        secure = true;
+    } else {
+        secure = false;
+    }
+
+    let transporter = await nodemailer.createTransport({
+        host: host,
+        port: port, //true for 465, false for other ports
+        secure: secure,
+        auth: {
+            user: user,
+            pass: password,
+        }
+    });
+
+    if (typemsg == 'text') {
+        try {
+            var info = await transporter.sendMail({
+                from: `"${name}" <${from}>`, // sender address
+                to: to, // list of receivers
+                subject: subject, // Subject line
+                text: message // plain text body
+            });
+        } catch (error) {
+            console.error(error);
+            errorResponse = error;
+        }
+    } else if (typemsg == 'html') {
+        try {
+            var info = await transporter.sendMail({
+                from: `"${name}" <${from}>`, // sender address
+                to: to, // list of receivers
+                subject: subject, // Subject line
+                html: message // html body
+            });
+        } catch (error) {
+            console.error(error);
+            errorResponse = error;
+        }
+    }
+
+    console.info("Message sent: %s", info.messageId);
+    // Message sent ID
+}
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+app.get('/api/v1/', (req, res) => {
+    res.send('POST requests only!');
+});
+
+app.post('/api/v1', async (req, res) => {
+    const response = await req.body;
+    try {
+        await sendEmail(
+            response.host,
+            response.port,
+            response.secure,
+            response.user,
+            response.password,
+            response.name,
+            response.from,
+            response.to,
+            response.subject,
+            response.typeMSG,
+            response.message
+        );
+    } catch (error) {
+        if (errorResponse.responseCode == 534) {
+            res.status(400).send(`
+            Error sending the mail:
+            Code: ${errorResponse.code}
+            Response: ${errorResponse.response || errorResponse}
+            Response code: ${errorResponse.responseCode}
+            If you are using Gmail SMTP you must enable Access from unsecured applications.
+            You can enable it here: https://myaccount.google.com/lesssecureapps
+    
+            Check the required fields or contact the administrator (github.com/alanescarcha).
+            `);
+        }
+        res.status(400).send(`
+        Error sending the mail:
+        Code: ${errorResponse.code || 'None'}
+        Response: ${errorResponse.response || errorResponse}
+        Response code: ${errorResponse.responseCode || 'None'}
+
+        Check the required fields or contact the administrator (github.com/alanescarcha).
+        `);
+        console.error(`Error app.post: ${error}`);
+    }
+    res.send('Email sent successfully!');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
